db9d8cdf35b43d9a98e8d68e7c397770ede7c9ac
Tad-Abraham <108827942+Eshkim@users.noreply.github.com>
2024-12-02 00:33:40 +0100

    carp1 revised

--- src/Pages/Cart/Cart.jsx
+++ src/Pages/Cart/Cart.jsx
@@ -1,59 +1,21 @@
-// // import classes from "./Cart.modules.css";
-// import { useContext } from "react";
-// import LayOut from "../../Components/LayOut/LayOut";
-// import ProductCard from "../../Components/Product/ProductCard";
-
-// function Cart() {
-//   const [{basket, user}, disppatch] = useContext(DataContext);
-  
-//   return (
-//     <LayOut>
-//       <section>
-//         <div>
-//           <h2>Hello</h2>
-//           <h3>Your Shopping basket</h3>
-//           <hr />
-//           {basket?.length === 0 ? (
-//             <p>! No item in your cart</p>
-//           ) : (
-//             basket?.map((item, i) => (
-//               <ProductCard
-//                 key={i}
-//                 product={item}
-//                 renderDesc={true}
-//                 flex={true}
-//               />
-//             ))
-//           )}
-//         </div>
-
-//         <div></div>
-//       </section>
-//     </LayOut>
-//   );
-// }
-
-// export default Cart;
-
-
-import  { useContext } from "react";
+import { useContext } from "react";
 import LayOut from "../../Components/LayOut/LayOut";
 import { DataContext } from "../../Components/DataProvider/DataProvider";
 
 import ProductCard from "../../Components/Product/ProductCard";
 import { Link } from "react-router-dom";
+import CurrencyFormat from "../../Components/CurrencyFormat/CurrencyFormat";
 
-
+//  import classes from "./Cart.modules.css";
 
 function Cart() {
-  
-  
-  const [{ basket, user } = dispatch] = useContext(DataContext);; // Destructure basket and user from state
+  const [{ basket, user } = dispatch] = useContext(DataContext); // Destructure basket and user from state
+  const total = basket?.reduce((amount, item) => item.price + amount, 0);
 
   return (
     <LayOut>
-      <section>
-        <div>
+      <section className={classes.container}>
+        <div className={classes.cart__container}>
           <h2>Hello</h2>
           <h3>Your Shopping basket</h3>
           <hr />
@@ -71,11 +33,11 @@ function Cart() {
             ))
           )}
         </div>
-        {/* {basket?.length !== 0 && (
-          <div>
+        {basket?.length !== 0 && (
+          <div className={classes.subtotal}>
             <div>
               <p>Subtotal ({basket?.length} items)</p>
-              <CurrencyFormat amount="total" />
+              <CurrencyFormat amount={total} />
             </div>
             <span>
               <input type="checkbox" />
@@ -83,8 +45,7 @@ function Cart() {
               <Link to="/Payment">Continue to checkout</Link>
             </span>
           </div>
-        )} */}
-       
+        )}
       </section>
     </LayOut>
   );
