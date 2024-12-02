073daa13229df65857cc36237cbdc9972bd174d6
Your Name <you@example.com>
2024-12-02 22:29:48 +0100

    car-multiple-item

--- src/Pages/Cart/Cart.jsx
+++ src/Pages/Cart/Cart.jsx
@@ -5,56 +5,59 @@ import { DataContext } from "../../Components/DataProvider/DataProvider";
 import ProductCard from "../../Components/Product/ProductCard";
 import { Link } from "react-router-dom";
 import CurrencyFormat from "../../Components/CurrencyFormat/CurrencyFormat";
-import classes from "./Cart.module.css"; // Import CSS module
+import classes from "./Cart.module.css";
 
 function Cart() {
   const [{ basket, user }, dispatch] = useContext(DataContext); // Destructure basket and user from state
-  const total = basket?.reduce((amount, item) => item.price + amount, 0);
+  const total = basket?.reduce(
+    (amount, item) => item.price * item.amount + amount,
+    0
+  );
 
   return (
     <LayOut>
-      <section className={classes.container}>
-        <div className={classes.cart__container}>
-          <h2>Hello, {user?.name || "Guest"}</h2>
-          <h3>Your Shopping Basket</h3>
-          <hr />
-          {basket?.length === 0 ? (
-            <div className={classes.emptyCart}>
-              <p>No items in your cart</p>
-              <Link to="/" className={classes.Start_Shopping}>
-                Start Shopping
-              </Link>
-            </div>
-          ) : (
-            basket?.map((item, i) => (
-              <ProductCard
-                key={i}
-                product={item}
-                renderDesc={true}
-                renderAdd={false}
-                flex={true}
-              />
-            ))
-          )}
-        </div>
+      <div className={classes.mainContainer}>
+        <section className={classes.container}>
+          <div className={classes.cart__container}>
+            <h2>Hello, {user?.name || "Guest"}</h2>
+            <h3>Your Shopping Basket</h3>
+            <hr />
+            {basket?.length === 0 ? (
+              <div className={classes.emptyCart}>
+                <p>No items in your cart</p>
+                <Link to="/" className={classes.Start_Shopping}>
+                  Start Shopping
+                </Link>
+              </div>
+            ) : (
+              basket?.map((item, i) => (
+                <ProductCard
+                  key={i}
+                  product={item}
+                  renderDesc={true}
+                  renderAdd={false}
+                  flex={true}
+                />
+              ))
+            )}
+          </div>
+        </section>
         {basket?.length !== 0 && (
           <div className={classes.subtotal}>
             <div className={classes.subtotal__details}>
               <p>Subtotal ({basket?.length} items)</p>
               <CurrencyFormat amount={total} />
             </div>
-            <div >
+            <div>
               <input type="checkbox" id="gift" />
               <label htmlFor="gift">
                 <small>This order contains a gift</small>
               </label>
             </div>
-            <Link to="/Payment" >
-              Continue to checkout
-            </Link>
+            <Link to="/Payment">Continue to checkout</Link>
           </div>
         )}
-      </section>
+      </div>
     </LayOut>
   );
 }
@@ -62,116 +65,108 @@ function Cart() {
 export default Cart;
 
 
-// import { useContext } from "react";
-// import LayOut from "../../Components/LayOut/LayOut";
-// import { DataContext } from "../../Components/DataProvider/DataProvider";
+// // import classes from "./Cart.module.css"; // Import CSS module
 
-// import ProductCard from "../../Components/Product/ProductCard";
-// import { Link } from "react-router-dom";
-// import CurrencyFormat from "../../Components/CurrencyFormat/CurrencyFormat";
+// // function Cart() {
+// //   const [{ basket, user }, dispatch] = useContext(DataContext); // Destructure basket and user from state
+// //   const total = basket?.reduce((amount, item) => item.price + amount, 0);
 
-// import classes from "./Cart.module.css"; // Import CSS module
+// //   return (
+// //     <LayOut>
+// //       <section className={classes.container}>
+// //         <div className={classes.cart__container}>
+// //           <h2>Hello</h2>
+// //           <h3>Your Shopping basket</h3>
+// //           <hr />
+// //           {basket?.length === 0 ? (
+// //             <p>! No item in your cart</p>
+// //           ) : (
+// //             basket?.map((item, i) => (
+// //               <ProductCard
+// //                 key={i}
+// //                 product={item}
+// //                 renderDesc={true}
+// //                 renderAdd={false}
+// //                 flex={true}
+// //               />
+// //             ))
+// //           )}
+// //         </div>
+// //         {basket?.length !== 0 && (
+// //           <div className={classes.subtotal}>
+// //             <div>
+// //               <p>Subtotal ({basket?.length} items)</p>
+// //               <CurrencyFormat amount={total} />
+// //             </div>
+// //             <span>
+// //               <input type="checkbox" />
+// //               <small>This order contains a gift</small>
+// //               <Link to="/Payment">Continue to checkout</Link>
+// //             </span>
+// //           </div>
+// //         )}
+// //       </section>
+// //     </LayOut>
+// //   );
+// // }
 
-// function Cart() {
-//   const [{ basket, user }, dispatch] = useContext(DataContext); // Destructure basket and user from state
-//   const total = basket?.reduce((amount, item) => item.price + amount, 0);
-
-//   return (
-//     <LayOut>
-//       <section className={classes.container}>
-//         <div className={classes.cart__container}>
-//           <h2>Hello</h2>
-//           <h3>Your Shopping basket</h3>
-//           <hr />
-//           {basket?.length === 0 ? (
-//             <p>! No item in your cart</p>
-//           ) : (
-//             basket?.map((item, i) => (
-//               <ProductCard
-//                 key={i}
-//                 product={item}
-//                 renderDesc={true}
-//                 renderAdd={false}
-//                 flex={true}
-//               />
-//             ))
-//           )}
-//         </div>
-//         {basket?.length !== 0 && (
-//           <div className={classes.subtotal}>
-//             <div>
-//               <p>Subtotal ({basket?.length} items)</p>
-//               <CurrencyFormat amount={total} />
-//             </div>
-//             <span>
-//               <input type="checkbox" />
-//               <small>This order contains a gift</small>
-//               <Link to="/Payment">Continue to checkout</Link>
-//             </span>
-//           </div>
-//         )}
-//       </section>
-//     </LayOut>
-//   );
-// }
-
-// export default Cart;
+// // export default Cart;
 
 
-// import { useContext } from "react";
-// import LayOut from "../../Components/LayOut/LayOut";
-// import { DataContext } from "../../Components/DataProvider/DataProvider";
+// // import { useContext } from "react";
+// // import LayOut from "../../Components/LayOut/LayOut";
+// // import { DataContext } from "../../Components/DataProvider/DataProvider";
 
-// import ProductCard from "../../Components/Product/ProductCard";
-// import { Link } from "react-router-dom";
-// import CurrencyFormat from "../../Components/CurrencyFormat/CurrencyFormat";
+// // import ProductCard from "../../Components/Product/ProductCard";
+// // import { Link } from "react-router-dom";
+// // import CurrencyFormat from "../../Components/CurrencyFormat/CurrencyFormat";
 
-// // import classes from "./Cart.modules.css";
+// // // import classes from "./Cart.modules.css";
 
-// function Cart() {
+// // function Cart() {
   
-//   const [{ basket, user } = dispatch] = useContext(DataContext); // Destructure basket and user from state
-//   // const { basket, user } = useContext(DataContext);
-//   const total = basket?.reduce((amount, item) => item.price + amount, 0);
+// //   const [{ basket, user } = dispatch] = useContext(DataContext); // Destructure basket and user from state
+// //   // const { basket, user } = useContext(DataContext);
+// //   const total = basket?.reduce((amount, item) => item.price + amount, 0);
 
-//   return (
-//     <LayOut>
-//       <section >
-//         <div>
+// //   return (
+// //     <LayOut>
+// //       <section >
+// //         <div>
 
-//           <h2>Hello</h2>
-//           <h3>Your Shopping basket</h3>
-//           <hr />
-//           {basket?.length === 0 ? (
-//             <p>! No item in your cart</p>
-//           ) : (
-//             basket?.map((item, i) => (
-//               <ProductCard
-//                 key={i}
-//                 product={item}
-//                 renderDesc={true}
-//                 renderAdd={false}
-//                 flex={true}
-//               />
-//             ))
-//           )}
-//         </div>
-//         {basket?.length !== 0 && (
-//           <div >
-//             <div>
-//               <p>Subtotal ({basket?.length} items)</p>
-//               <CurrencyFormat amount={total} />
-//             </div>
-//             <span>
-//               <input type="checkbox" />
-//               <small>This order contains a gift</small>
-//               <Link to="/Payment">Continue to checkout</Link>
-//             </span>
-//           </div>
-//         )}
-//       </section>
-//     </LayOut>
-//   );
-// }
+// //           <h2>Hello</h2>
+// //           <h3>Your Shopping basket</h3>
+// //           <hr />
+// //           {basket?.length === 0 ? (
+// //             <p>! No item in your cart</p>
+// //           ) : (
+// //             basket?.map((item, i) => (
+// //               <ProductCard
+// //                 key={i}
+// //                 product={item}
+// //                 renderDesc={true}
+// //                 renderAdd={false}
+// //                 flex={true}
+// //               />
+// //             ))
+// //           )}
+// //         </div>
+// //         {basket?.length !== 0 && (
+// //           <div >
+// //             <div>
+// //               <p>Subtotal ({basket?.length} items)</p>
+// //               <CurrencyFormat amount={total} />
+// //             </div>
+// //             <span>
+// //               <input type="checkbox" />
+// //               <small>This order contains a gift</small>
+// //               <Link to="/Payment">Continue to checkout</Link>
+// //             </span>
+// //           </div>
+// //         )}
+// //       </section>
+// //     </LayOut>
+// //   );
+// // }
 
-// export default Cart;
+// // export default Cart;
--- src/Utility/reducer.js
+++ src/Utility/reducer.js
@@ -1,3 +1,37 @@
+// import { Type } from "./action.type";
+
+// export const initialState = {
+//   basket: [],
+// };
+
+// export const reducer = (state, action) => {
+//   switch (action.type) {
+//     case Type.ADD_TO_BASKET:
+//       // Check if item already exists in the basket, if yes increment the amount, else add the new item with amount 1.
+//       const existingItem = state.basket.find((item) => item.id === action.item.id);
+//       if(!existingItem)
+//         return {
+//          ...state,
+//           basket: [...state.basket, {...action.item, amount: 1}],
+//         };
+//       else {
+//         {
+//           const updatedBasket = [...state.basket];
+//           const index = updatedBasket.map((item) =>{
+//             item.id === action.item.id?{...item, amount: item.amount + 1}:item
+//           } )
+//           return {...state, basket: updatedBasket};
+
+//           // return {
+//       //   ...state,
+//       //   basket: [...state.basket, action.item], // Append the new item to the basket array
+//       // };
+    
+//       default:
+//       return state;
+//   }
+// };
+
 import { Type } from "./action.type";
 
 export const initialState = {
@@ -7,10 +41,22 @@ export const initialState = {
 export const reducer = (state, action) => {
   switch (action.type) {
     case Type.ADD_TO_BASKET:
-      return {
-        ...state,
-        basket: [...state.basket, action.item], // Append the new item to the basket array
-      };
+      // Check if item already exists in the basket, if yes increment the amount, else add the new item with amount 1.
+      const existingItem = state.basket.find(
+        (item) => item.id === action.item.id
+      );
+      if (!existingItem) {
+        return {
+          ...state,
+          basket: [...state.basket, { ...action.item, amount: 1 }],
+        };
+      } else {
+        const updatedBasket = state.basket.map((item) =>{
+          return item.id === action.item.id? {...item, amount: item.amount + 1 } : item;
+        });
+        return { ...state, basket: updatedBasket };
+      }
+
     default:
       return state;
   }
