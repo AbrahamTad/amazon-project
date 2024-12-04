cad2174f4be8228a982a34bf977229782a475f64
Your Name <you@example.com>
2024-12-04 16:15:17 +0100

    Secure API keys using environment variables

--- src/Utility/firebase.js
+++ src/Utility/firebase.js
@@ -1,21 +1,19 @@
-
 import firebase from "firebase/compat/app";
 import { getAuth } from "firebase/auth";
 import "firebase/compat/firestore";
 import "firebase/compat/auth";
 
-// Your web app's Firebase configuration
+// Your web app's Firebase configuration using environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyCOMOMSeWcsm3HVUTgNmuXeI7CokvcVpIw",
-  authDomain: "clone-cfd41.firebaseapp.com",
-  projectId: "clone-cfd41",
-  storageBucket: "clone-cfd41.firebasestorage.app",
-  messagingSenderId: "306981946234",
-  appId: "1:306981946234:web:40a2da3b1d686a3b588cc3",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
 const app = firebase(firebaseConfig);
-export const auth = getAuth(app); 
+export const auth = getAuth(app);
 export const db = app.firestore();
-
