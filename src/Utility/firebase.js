21dd4ff1b9295f66ecb6bda0ba35deab832386a9
Tad-Abraham <108827942+Eshkim@users.noreply.github.com>
2024-12-04 11:20:15 +0100

    Setting up Firebase

---
+++ src/Utility/firebase.js
@@ -0,0 +1,21 @@
+
+import firebase from "firebase/compat/app";
+import { getAuth } from "firebase/auth";
+import "firebase/compat/firestore";
+import "firebase/compat/auth";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCOMOMSeWcsm3HVUTgNmuXeI7CokvcVpIw",
+  authDomain: "clone-cfd41.firebaseapp.com",
+  projectId: "clone-cfd41",
+  storageBucket: "clone-cfd41.firebasestorage.app",
+  messagingSenderId: "306981946234",
+  appId: "1:306981946234:web:40a2da3b1d686a3b588cc3",
+};
+
+// Initialize Firebase
+const app = firebase(firebaseConfig);
+export const auth = getAuth(app); 
+export const db = app.firestore();
+
