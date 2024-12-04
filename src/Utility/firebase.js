55841ba230c4fafaf651929e2e4f3c24a4f26094
Tad-Abraham <108827942+Eshkim@users.noreply.github.com>
2024-12-04 22:13:53 +0100

    css-cart-update

--- src/Utility/firebase.js
+++ src/Utility/firebase.js
@@ -1,21 +1,55 @@
+/* eslint-disable no-undef */
 
 import firebase from "firebase/compat/app";
 import { getAuth } from "firebase/auth";
 import "firebase/compat/firestore";
 import "firebase/compat/auth";
+import dotenv from "dotenv"; // Import dotenv
 
-// Your web app's Firebase configuration
+dotenv.config(); // Load environment variables
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCOMOMSeWcsm3HVUTgNmuXeI7CokvcVpIw",
-  authDomain: "clone-cfd41.firebaseapp.com",
-  projectId: "clone-cfd41",
-  storageBucket: "clone-cfd41.firebasestorage.app",
-  messagingSenderId: "306981946234",
-  appId: "1:306981946234:web:40a2da3b1d686a3b588cc3",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
-// Initialize Firebase
 const app = firebase(firebaseConfig);
-export const auth = getAuth(app); 
+export const auth = getAuth(app);
 export const db = app.firestore();
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import firebase from "firebase/compat/app";
+// import { getAuth } from "firebase/auth";
+// import "firebase/compat/firestore";
+// import "firebase/compat/auth";
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "xx",
+//   authDomain: "clone-cfd41.firebaseapp.com",
+//   projectId: "clone-cfd41",
+//   storageBucket: "clone-cfd41.firebasestorage.app",
+//   messagingSenderId: "306981946234",
+//   appId: "1:306981946234:web:40a2da3b1d686a3b588cc3",
+// };
+
+// // Initialize Firebase
+// const app = firebase(firebaseConfig);
+// export const auth = getAuth(app); 
+// export const db = app.firestore();
 
