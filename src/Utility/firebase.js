3dfe79e42af566e46e87c80edeb5b3c1084bda76
Tad-Abraham <108827942+Eshkim@users.noreply.github.com>
2024-12-06 10:47:55 +0100

    gitingnore &firebase-utility

--- src/Utility/firebase.js
+++ src/Utility/firebase.js
@@ -1,50 +1,22 @@
-
 import firebase from "firebase/compat/app";
 
 import { getAuth } from "firebase/auth";
 import "firebase/compat/firestore";
 import "firebase/compat/auth";
 
-
-// Your web app's Firebase configuration
+// web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCOMOMSeWcsm3HVUTgNmuXeI7CokvcVpIw",
-  authDomain: "clone-cfd41.firebaseapp.com",
-  projectId: "clone-cfd41",
-  storageBucket: "clone-cfd41.firebasestorage.app",
-  messagingSenderId: "306981946234",
-  appId: "1:306981946234:web:40a2da3b1d686a3b588cc3",
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
+
 // Initialize Firebase
-// const app = firebase(firebaseConfig);
-// export const auth = getAuth(app);
-// export const db = app.firestore();
 const app = !firebase.apps.length
   ? firebase.initializeApp(firebaseConfig)
   : firebase.app();
 export const auth = getAuth(app);
 export const db = firebase.firestore;
-
-
-// import firebase from "firebase/compat/app";
-// import { getAuth } from "firebase/auth";
-// import "firebase/compat/firestore";
-// import "firebase/compat/auth";
-
-
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCOMOMSeWcsm3HVUTgNmuXeI7CokvcVpIw",
-//   authDomain: "clone-cfd41.firebaseapp.com",
-//   projectId: "clone-cfd41",
-//   storageBucket: "clone-cfd41.firebasestorage.app",
-//   messagingSenderId: "306981946234",
-//   appId: "1:306981946234:web:40a2da3b1d686a3b588cc3"
-// };
-
-// // Initialize Firebase
-// const app = firebase.initializeApp(firebaseConfig);
-// export const auth = getAuth(app);
-// export const db = firebase.firestore();
-
-
