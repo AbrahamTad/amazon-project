a76697c36c2ad5413993291293746cf61314222f
Tad-Abraham <108827942+Eshkim@users.noreply.github.com>
2024-12-09 20:59:20 +0100

    deploying to netlify

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 const axiosInstance = axios.create({
+  //local instance of firebase function
   // baseURL: "http://127.0.0.1:5001/clone-cfd41/us-central1/api",
+  //deployed version of firebase function
   baseURL: "https://api-rzohlyxfbq-uc.a.run.app/",
+  //deployed version of amazon server on render.com
+    // baseURL: "https://amazon-api-deploy-f8b9.onrender.com/",
 });
 
 export default axiosInstance;
